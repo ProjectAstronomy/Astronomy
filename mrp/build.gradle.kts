49aaccbee68a7adc526a36775b796df854b64efe
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2022-04-14 18:25:39 -0400

    Create :mrp module

---
+++ mrp/build.gradle.kts
@@ -0,0 +1,15 @@
+android {
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
