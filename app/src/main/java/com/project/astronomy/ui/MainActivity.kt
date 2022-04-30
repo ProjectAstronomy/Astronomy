ac63e6672effc27029d35ee90455805bf6259bc6
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2022-04-30 17:12:11 -0400

    Code review #2

--- app/src/main/java/com/project/astronomy/ui/MainActivity.kt
+++ app/src/main/java/com/project/astronomy/ui/MainActivity.kt
@@ -7,16 +7,17 @@ import androidx.navigation.fragment.NavHostFragment
 import com.project.astronomy.R
 
 class MainActivity : AppCompatActivity() {
-    private val navHostFragment =
-        supportFragmentManager.findFragmentById(R.id.activity_main_container) as NavHostFragment
-    private val navController: NavController = navHostFragment.navController
+    private var navController: NavController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.activity_main_container) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController?.navigateUp() == true || super.onSupportNavigateUp()
     }
 }
